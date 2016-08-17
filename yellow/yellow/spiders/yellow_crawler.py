c531c30d4e3140e9a111decfc70b4ebb382b808a
Henning Steinke <henning85@gmail.com>
2016-08-17 21:40:15 +0200

    Initial commit

---
+++ yellow/yellow/spiders/yellow_crawler.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.selector import Selector
+
+from yellow.items import YellowItem
+
+
+class YellowCrawlerSpider(CrawlSpider):
+    name = 'yellow_crawler'
+    allowed_domains = ['gelbeseiten.de']
+    start_urls = [
+        "http://www.gelbeseiten.de/wohnungsverwaltung/berlin"
+    ]
+
+    rules = (
+        Rule(SgmlLinkExtractor(allow=(), restrict_xpaths=('//div[@class="gs_seite_vor_wrapper"]/a')), callback="parse_item", follow=True),
+    )
+
+    def parse_item(self, response):
+        for box in response.xpath('//div[@class="box"]'):
+            item = YellowItem()
+            item['name'] = box.xpath('div//span[@itemprop="name"]/text()').extract()
+            item['streetAddress'] = box.xpath('div//span[@itemprop="streetAddress"]/text()').extract()
+            item['postalCode'] = box.xpath('div//span[@itemprop="postalCode"]/text()').extract()
+            item['addressLocality'] = box.xpath('div//span[@itemprop="addressLocality"]/text()').extract()
+            item['mail'] = box.xpath('div//span[@itemprop="email"]/text()').extract()
+            item ['website'] = box.xpath('div//div[contains(@class, "B")]//li[contains(@class, "website")]/a/span/text()').extract()
+            yield item
---
+++ yellow/yellow/spiders/yellow_spider.py
@@ -0,0 +1,22 @@
+from scrapy import Spider
+from scrapy.selector import Selector
+
+from yellow.items import YellowItem
+
+class YellowSpider(Spider):
+	name = "yellow"
+	allowed_domains = ["gelbeseiten.de"]
+	start_urls = [
+		"http://www.gelbeseiten.de/wohnungsverwaltung/berlin",
+	]
+
+	def parse(self, response):
+		for box in response.xpath('//div[@class="box"]'):
+			item = YellowItem()
+			item['name'] = box.xpath('div//span[@itemprop="name"]/text()').extract()
+			item['streetAddress'] = box.xpath('div//span[@itemprop="streetAddress"]/text()').extract()
+			item['postalCode'] = box.xpath('div//span[@itemprop="postalCode"]/text()').extract()
+			item['addressLocality'] = box.xpath('div//span[@itemprop="addressLocality"]/text()').extract()
+			item['mail'] = box.xpath('div//span[@itemprop="email"]/text()').extract()
+			item ['website'] = box.xpath('div//div[contains(@class, "B")]//li[contains(@class, "website")]/a/span/text()').extract()
+			yield item
