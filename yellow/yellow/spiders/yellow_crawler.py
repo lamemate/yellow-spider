69bc4d46cf924f86d2ce34863f9746715a92dda1
Henning Steinke <henning85@gmail.com>
2016-08-17 23:05:54 +0200

    Adds more keywords

--- yellow/yellow/spiders/yellow_crawler.py
+++ yellow/yellow/spiders/yellow_crawler.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+from scrapy.http import Request
 from scrapy.selector import Selector
 
 from yellow.items import YellowItem
@@ -11,11 +12,13 @@ class YellowCrawlerSpider(CrawlSpider):
     name = 'yellow_crawler'
     allowed_domains = ['gelbeseiten.de']
     start_urls = [
-        "http://www.gelbeseiten.de/wohnungsverwaltung/berlin"
+        "http://www.gelbeseiten.de/wohnungsverwaltung/berlin",
+        "http://www.gelbeseiten.de/wohnungsgenossenschaften/berlin",
+        "http://www.gelbeseiten.de/wohnungsgesellschaften/berlin",
     ]
 
     rules = (
-        Rule(SgmlLinkExtractor(allow=(), restrict_xpaths=('//div[@class="gs_seite_vor_wrapper"]/a')), callback="parse_item", follow=True),
+        Rule(LinkExtractor(allow=(), restrict_xpaths=('//div[@class="gs_seite_vor_wrapper"]/a')), callback="parse_item", follow=True),
     )
 
     def parse_item(self, response):
@@ -26,5 +29,5 @@ class YellowCrawlerSpider(CrawlSpider):
             item['postalCode'] = box.xpath('div//span[@itemprop="postalCode"]/text()').extract()
             item['addressLocality'] = box.xpath('div//span[@itemprop="addressLocality"]/text()').extract()
             item['mail'] = box.xpath('div//span[@itemprop="email"]/text()').extract()
-            item ['website'] = box.xpath('div//div[contains(@class, "B")]//li[contains(@class, "website")]/a/span/text()').extract()
+            item['website'] = box.xpath('div//div[contains(@class, "B")]//li[contains(@class, "website")]/a/span/text()').extract()
             yield item
